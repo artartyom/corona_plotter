188f05ab9c552172d4d229a1612a30245d54b469
Artem Shevlyakov <artartyom@gmail.com>
2020-01-31 18:22:31 +0300

    Initial commit

---
+++ corona.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+Rscript makePlot.R
+eog corona.png
---
+++ makePlot.R
@@ -0,0 +1,27 @@
+library(data.table)
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+
+corona <- fread("./coronaData.tsv", data.table = F)
+corona$date <- dmy(corona$date)
+corona <- rbind(
+                data.table(date = min(corona$date),
+                           diseased = 0,
+                           died = 0),
+                corona,
+                data.table(date = max(corona$date),
+                diseased = 0,
+                died = 0
+                )
+)
+corona_gathered <- gather(corona, "type", "cases", -date)
+
+g <- ggplot(corona_gathered, mapping = aes(x = date, y = cases, color = type, fill = type))
+
+png("corona.png", width = 600, height = 600)
+g + 
+    geom_polygon(alpha = 0.1) + 
+    scale_x_date() + 
+    scale_y_log10()
+dev.off()
